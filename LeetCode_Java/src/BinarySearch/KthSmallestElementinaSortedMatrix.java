388bcb23ac240d55c386bbfad5b58ec0c00bfc3b
ChencongDiu <chencong@gwmail.gwu.edu>
2017-12-25 22:49:43 -0500

    optimize, binary search

--- LeetCode_Java/src/BinarySearch/KthSmallestElementinaSortedMatrix.java
+++ LeetCode_Java/src/BinarySearch/KthSmallestElementinaSortedMatrix.java
@@ -1,7 +1,5 @@
 package BinarySearch;
 
-import java.util.Arrays;
-
 /*
 Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
 
