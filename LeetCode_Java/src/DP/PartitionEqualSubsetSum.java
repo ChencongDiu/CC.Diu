d755910026ba162f5b5e6726af95eb2fc94e28a8
ChencongDiu <chencong@gwmail.gwu.edu>
2017-12-28 13:03:23 -0500

    optimize 416

--- LeetCode_Java/src/DP/PartitionEqualSubsetSum.java
+++ LeetCode_Java/src/DP/PartitionEqualSubsetSum.java
@@ -31,10 +31,8 @@ public class PartitionEqualSubsetSum {
         boolean[] dp = new boolean[sum + 1];
         dp[0] = true;
         for (int n: nums) {
-            for (int i = sum; i >= 0; i--) {
-                if (i - n >= 0) {
-                    dp[i] = dp[i] || dp[i - n]; //all reached number + n will also be reached
-                }
+            for (int i = sum; i >= n; i--) {
+                dp[i] = dp[i] || dp[i - n]; //all reached number + n will also be reached
             }
         }
         return dp[sum];
